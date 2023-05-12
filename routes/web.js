3d6d9d7f369fd96426bc0228870ae8d009862093
zZaala <zararanwar1234321@gmail.com>
2023-05-13 01:08:22 +0500

    Updates

--- routes/web.js
+++ routes/web.js
@@ -5,6 +5,7 @@ const router=express.Router()
 router.get("/",(req,res)=> res.render('home'))
 router.get("/service",(req,res)=> res.render('service'))
 router.get("/contact",(req,res)=> res.render('contact'))
+router.get("*",(req,res)=> res.render('404'))
 
 
 module.exports=router
