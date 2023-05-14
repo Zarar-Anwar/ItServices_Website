366be4c720b96580d563966689860bf192ebf21b
zZaala <zararanwar1234321@gmail.com>
2023-05-14 14:37:47 +0500

    2nd Updates

--- routes/web.js
+++ routes/web.js
@@ -2,10 +2,9 @@ const express=require("express")
 
 const router=express.Router()
 
-router.get("/",(req,res)=> res.render('home'))
-router.get("/service",(req,res)=> res.render('service'))
-router.get("/contact",(req,res)=> res.render('contact'))
-router.get("*",(req,res)=> res.render('404'))
+router.get("/",(req,res)=> res.render('home',{title:"SamTech | Home"}))
+router.get("/contact",(req,res)=> res.render('contact',{title:"SamTech | Contact"}))
+router.get("*",(req,res)=> res.render('404',{title:"404 Error"}))
 
 
 module.exports=router
